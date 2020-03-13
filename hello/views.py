efd34a2bca329c39d2ac233091429d4ec6e99cf5
ttlevt <ttlevt@gmail.com>
2020-03-13 10:33:31 +0900

    test_django

---
+++ hello/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.http import JsonResponse
+# def home (request):
+#     return HttpResponse("Hello, Django!!")
+def home(request):
+    name = request.GET['name']
+    age = request.GET['age']
+    getRequestDict = request.GET;
+    return JsonResponse(getRequestDict)
+
